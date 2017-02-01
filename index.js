3d2e1aea8bf03d607d2b91e208def8b28c43b834
Brian Moran <BMoran@urbn.com>
2017-02-01 13:57:37 -0500

    Adjust colors

--- index.js
+++ index.js
@@ -3,21 +3,16 @@
 const foregroundColor = '#cedde0'
 const backgroundColor = '#2f3542'
 const black = backgroundColor
-const slate = '#5b5a77'
-const lightSlate = '#767594'
 const blue = '#44C0C6'
 const orange = '#FC8458'
-const lightOrange = '#FC8458'
 const gray = '#3d424b'
 const brightBlack = backgroundColor
 const brightWhite = foregroundColor
-const red = '#8c4646'
-const green = '#cae359'
 
 const colors = {
 	black: backgroundColor,
-	red: red,
-	green: green,
+	red: blue,
+	green: orange,
 	yellow: blue,
 	blue: orange,
 	magenta: blue,
@@ -25,7 +20,7 @@ const colors = {
 	white: orange,
 	lightBlack: backgroundColor,
 	lightRed: orange,
-	lightGreen: green,
+	lightGreen: orange,
 	lightYellow: blue,
 	lightBlue: blue,
 	lightMagenta: blue,
@@ -46,7 +41,7 @@ exports.decorateConfig = config => {
                 border: 0;
             }
             .tab_textActive {
-                border-bottom: 2px solid ${blue};
+                border-bottom: 2px solid ${orange};
             }
 		`
 	})
